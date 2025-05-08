e03fa77c923d56369cc28820b8bde452b1635976
Ex-Lord-Ryu <135092655+Ex-Lord-Ryu@users.noreply.github.com>
2025-05-08 16:02:11 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,19 +15,12 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'admin',
-            'email' => 'ranggayuda2003@gmail.com',
-            'password' => bcrypt('admin123'),
-            'role' => 'admin',
-        ]);
-
-        User::factory()->create([
-            'name' => 'admin2',
-            'email' => 'josuaedison6@gmail.com',
-            'password' => bcrypt('admin123'),
-            'role' => 'admin',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'admin',
+        //     'email' => 'example@gmail.com',
+        //     'password' => bcrypt('admin123'),
+        //     'role' => 'admin',
+        // ]);
 
         $this->call(LocationsSeeder::class);
     }
